af5256332319f578203437fcfa7c654705814c2b
EduardoMilla <eduardo.milla@gmail.com>
2020-04-15 13:47:03 -0500

    Actualizacion de reportes

--- QUERYS_ORACLE/RELACIONES_BUSQUEDA.sql
+++ QUERYS_ORACLE/RELACIONES_BUSQUEDA.sql
@@ -11,7 +11,7 @@ SELECT a.table_name, a.column_name, a.constraint_name, c.owner,
   JOIN all_constraints c_pk ON c.r_owner = c_pk.owner
                            AND c.r_constraint_name = c_pk.constraint_name
 WHERE c.constraint_type = 'R'
-   AND a.table_name like '%ORD_EQUIPMENT_ORDER_ITEMS%'
+   AND a.table_name like '%VSL_VESSELS%'
    ORDER BY R_TABLE_NAME;
 --REF_EQUIPMENT
 
@@ -40,7 +40,8 @@ WHERE c.constraint_type = 'R'
 
 
 select column_name, data_length, data_type from all_tab_columns 
-where table_name = 'REF_EQUIPMENT';
+where table_name = 'INV_UNIT'
+AND COLUMN_NAME LIKE '%STRING08%';
 
 
 select * FROM all_cons_columns 
